0ccd892d1ae0fa53add5bf64fc2d48c0c896533f
Jay King <jayriverking@gmail.com>
2023-04-21 10:11:25 -0400

    made routes and models directory

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,9 @@ from flask import Flask
 def create_app():
     # __name__ stores the name of the module we're in
     app = Flask(__name__)
+    # import amethyst_bp from amethyst.py file
+    # from .amethyst import amethyst_bp
+    # register blueprint
+    # app.register_blueprint(amethyst_bp)
 
     return app
---
+++ app/amethyst.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+
+# Make class Amethyst
+
+# Make Blueprint for an instance of Amethyst
+
+# amethyst_bp = Blueprint("", __name__, url_prefix="")
+
+# @amethyst_bp.routes("/", methods=["GET"])
+# def somefunction():
