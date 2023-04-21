7f92eab57b075cfd3aef0d1cc678e1b73b09332b
Jay King <jayriverking@gmail.com>
2023-04-21 10:30:43 -0400

    removed redundancy

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,9 @@ from flask import Flask
 def create_app():
     # __name__ stores the name of the module we're in
     app = Flask(__name__)
-    # import amethyst_bp from amethyst.py file
-    # from .amethyst import amethyst_bp
-    # register blueprint
-    # app.register_blueprint(amethyst_bp)
+    # import crystal_bp from routes.py file
+    from .routes import crystal_bp
+    app.register_blueprint(crystal_bp)
+
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,26 @@
 # this is the routes.py file substitute (if there's multiple classes for multiple routes, you'd want a directory/file for keeping all of them together)
+
 from flask import Blueprint
 
 # Make class Crystal
 class Crystal:
+    def __init__(self, id, name, color, powers):
+        self.id = id
+        self.name = name
+        self.color = color
+        self.powers = powers
+# create a list of crystals
 
-# Make Blueprint for an instance of Amethyst
+crystals = [
+    Crystal(1, "Amethyst", "Purple", "Infinite Knowledge & Wisdom"),
+    Crystal(2, "Tiger's Eye", "Gold", "Confidence, Strength, Intelligence"),
+    Crystal(3, "Rose Quartz", "Pink", "Love"),
+]
 
-# amethyst_bp = Blueprint("", __name__, url_prefix="")
 
-# @amethyst_bp.routes("/", methods=["GET"])
+# Make Blueprint for an instance of Crystal
+
+crystal_bp = Blueprint("crystals", __name__, url_prefix="/crystals")
+
+# @crystal_bp.routes("/", methods=["GET"])
 # def somefunction():
