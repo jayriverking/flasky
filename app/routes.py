32a91723ce6a074f51dd570fa089e2c363cfdbb6
Jay King <jayriverking@gmail.com>
2023-04-24 11:52:58 -0400

    april 24

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 # this is the routes.py file substitute (if there's multiple classes for multiple routes, you'd want a directory/file for keeping all of them together)
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 # Make class Crystal
 class Crystal:
@@ -16,6 +16,18 @@ crystals = [
     Crystal(2, "Tiger's Eye", "Gold", "Confidence, Strength, Intelligence"),
     Crystal(3, "Rose Quartz", "Pink", "Love"),
 ]
+
+# helper function to validate crystal id
+def validate_crystal(crystal_id):
+    try:
+        crystal_id = int(crystal_id)
+    except:
+        abort(make_response(({"message": f"Crystal {crystal_id} is not a valid input type. ({type(crystal_id)}. Must be an integer.)"}, 400)))
+    for crystal in crystals:
+        if crystal.id == crystal_id:
+            return crystal
+    abort(make_response({"message": f"Crystal id {crystal_id} does not exist"}, 404))
+
 
 
 # Make Blueprint for an instance of Crystal
@@ -34,3 +46,16 @@ def handle_crystals():
             "powers": crystal.powers
         })
     return jsonify(crystal_response)
+
+
+@crystal_bp.route("/<crystal_id>", methods=["GET"])
+def handle_crystal(crystal_id):
+    crystal = validate_crystal(crystal_id)
+    return {
+    "id": crystal.id,
+    "name": crystal.name,
+    "color": crystal.color,
+    "powers": crystal.powers
+    }
+    
+# jsonifying -- only needed when things like lists are returned and need to be formatted in json
