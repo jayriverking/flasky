82159b8ce99dbcd930480311e6aee8e9ddfe977b
Jay King <jayriverking@gmail.com>
2023-04-28 10:32:39 -0400

    add POST new crystal method

--- app/routes.py
+++ app/routes.py
@@ -1,62 +1,20 @@
 # this is the routes.py file substitute (if there's multiple classes for multiple routes, you'd want a directory/file for keeping all of them together)
-
-from flask import Blueprint, jsonify, abort, make_response
+from app import db
+from app.models.crystal import Crystal
+from flask import Blueprint, jsonify, abort, make_response, request
 
 crystal_bp = Blueprint("crystals", __name__, url_prefix="/crystals")
 
-# Make class Crystal
-# class Crystal:
-#     def __init__(self, id, name, color, powers):
-#         self.id = id
-#         self.name = name
-#         self.color = color
-#         self.powers = powers
+@crystal_bp.route("", methods=["POST"])
+def handle_crystals():
+    request_body = request.get_json()
+    new_crystal = Crystal(
+        name = request_body["name"],
+        color = request_body["color"],
+        powers = request_body["powers"]
+    )
+    db.session.add(new_crystal)
+    db.session.commit()
 
-# Create a list of crystals
-# crystals = [
-#     Crystal(1, "Amethyst", "Purple", "Infinite Knowledge & Wisdom"),
-#     Crystal(2, "Tiger's Eye", "Gold", "Confidence, Strength, Intelligence"),
-#     Crystal(3, "Rose Quartz", "Pink", "Love"),
-# ]
-
-# helper function to validate crystal id
-# def validate_crystal(crystal_id):
-    # try:
-    #     crystal_id = int(crystal_id)
-    # except:
-    #     abort(make_response(({"message": f"Crystal {crystal_id} is not a valid input type. ({type(crystal_id)}. Must be an integer.)"}, 400)))
-    # for crystal in crystals:
-    #     if crystal.id == crystal_id:
-    #         return crystal
-    # abort(make_response({"message": f"Crystal id {crystal_id} does not exist"}, 404))
-
-
-
-# Make Blueprint for an instance of Crystal
-
-
-# make a decorator & function for handling the route
-# @crystal_bp.route("", methods=["GET"])
-# def handle_crystals():
-#     crystal_response = []
-#     for crystal in crystals:
-#         crystal_response.append({
-#             "id": crystal.id,
-#             "name": crystal.name,
-#             "color": crystal.color,
-#             "powers": crystal.powers
-#         })
-#     return jsonify(crystal_response)
-
-
-# @crystal_bp.route("/<crystal_id>", methods=["GET"])
-# def handle_crystal(crystal_id):
-#     crystal = validate_crystal(crystal_id)
-#     return {
-#     "id": crystal.id,
-#     "name": crystal.name,
-#     "color": crystal.color,
-#     "powers": crystal.powers
-#     }
+    return make_response(f"Crystal {new_crystal.name} successfully created.", 201)
     
-# jsonifying -- only needed when things like lists are returned and need to be formatted in json
