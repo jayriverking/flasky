1cce13499be4ec5acadab84acda69f103986f6e1
Jay King <jayriverking@gmail.com>
2023-04-28 10:04:22 -0400

    add crystal db

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,26 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+# gives use access to database operations
+db = SQLAlchemy()
+migrate = Migrate()
+
 
 def create_app():
     # __name__ stores the name of the module we're in
     app = Flask(__name__)
+
+    # set up database
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/flasky_development'
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
     # import crystal_bp from routes.py file
     from .routes import crystal_bp
     app.register_blueprint(crystal_bp)
+
+    from app.models.crystal import Crystal
     # return the app
     return app
--- app/routes.py
+++ app/routes.py
@@ -2,60 +2,61 @@
 
 from flask import Blueprint, jsonify, abort, make_response
 
-# Make class Crystal
-class Crystal:
-    def __init__(self, id, name, color, powers):
-        self.id = id
-        self.name = name
-        self.color = color
-        self.powers = powers
-# create a list of crystals
+crystal_bp = Blueprint("crystals", __name__, url_prefix="/crystals")
 
-crystals = [
-    Crystal(1, "Amethyst", "Purple", "Infinite Knowledge & Wisdom"),
-    Crystal(2, "Tiger's Eye", "Gold", "Confidence, Strength, Intelligence"),
-    Crystal(3, "Rose Quartz", "Pink", "Love"),
-]
+# Make class Crystal
+# class Crystal:
+#     def __init__(self, id, name, color, powers):
+#         self.id = id
+#         self.name = name
+#         self.color = color
+#         self.powers = powers
+
+# Create a list of crystals
+# crystals = [
+#     Crystal(1, "Amethyst", "Purple", "Infinite Knowledge & Wisdom"),
+#     Crystal(2, "Tiger's Eye", "Gold", "Confidence, Strength, Intelligence"),
+#     Crystal(3, "Rose Quartz", "Pink", "Love"),
+# ]
 
 # helper function to validate crystal id
-def validate_crystal(crystal_id):
-    try:
-        crystal_id = int(crystal_id)
-    except:
-        abort(make_response(({"message": f"Crystal {crystal_id} is not a valid input type. ({type(crystal_id)}. Must be an integer.)"}, 400)))
-    for crystal in crystals:
-        if crystal.id == crystal_id:
-            return crystal
-    abort(make_response({"message": f"Crystal id {crystal_id} does not exist"}, 404))
+# def validate_crystal(crystal_id):
+    # try:
+    #     crystal_id = int(crystal_id)
+    # except:
+    #     abort(make_response(({"message": f"Crystal {crystal_id} is not a valid input type. ({type(crystal_id)}. Must be an integer.)"}, 400)))
+    # for crystal in crystals:
+    #     if crystal.id == crystal_id:
+    #         return crystal
+    # abort(make_response({"message": f"Crystal id {crystal_id} does not exist"}, 404))
 
 
 
 # Make Blueprint for an instance of Crystal
 
-crystal_bp = Blueprint("crystals", __name__, url_prefix="/crystals")
 
 # make a decorator & function for handling the route
-@crystal_bp.route("", methods=["GET"])
-def handle_crystals():
-    crystal_response = []
-    for crystal in crystals:
-        crystal_response.append({
-            "id": crystal.id,
-            "name": crystal.name,
-            "color": crystal.color,
-            "powers": crystal.powers
-        })
-    return jsonify(crystal_response)
+# @crystal_bp.route("", methods=["GET"])
+# def handle_crystals():
+#     crystal_response = []
+#     for crystal in crystals:
+#         crystal_response.append({
+#             "id": crystal.id,
+#             "name": crystal.name,
+#             "color": crystal.color,
+#             "powers": crystal.powers
+#         })
+#     return jsonify(crystal_response)
 
 
-@crystal_bp.route("/<crystal_id>", methods=["GET"])
-def handle_crystal(crystal_id):
-    crystal = validate_crystal(crystal_id)
-    return {
-    "id": crystal.id,
-    "name": crystal.name,
-    "color": crystal.color,
-    "powers": crystal.powers
-    }
+# @crystal_bp.route("/<crystal_id>", methods=["GET"])
+# def handle_crystal(crystal_id):
+#     crystal = validate_crystal(crystal_id)
+#     return {
+#     "id": crystal.id,
+#     "name": crystal.name,
+#     "color": crystal.color,
+#     "powers": crystal.powers
+#     }
     
 # jsonifying -- only needed when things like lists are returned and need to be formatted in json
