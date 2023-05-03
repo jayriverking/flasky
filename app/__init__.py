9dd5192fbfdcfb88fe01365228e5b43c70d2160d
Jay King <jayriverking@gmail.com>
2023-05-03 10:38:03 -0400

    add env variables and uri updates

--- app/__init__.py
+++ app/__init__.py
@@ -2,19 +2,31 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+# from env -- import uri's and stuff
+from dotenv import load_dotenv
+import os
+
 # gives use access to database operations
 db = SQLAlchemy()
 migrate = Migrate()
+load_dotenv()
 
 
-def create_app():
+def create_app(test_config=None):
     # __name__ stores the name of the module we're in
     app = Flask(__name__)
 
     # set up database
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/flasky_development'
-    
+    if not test_config:
+        # development environment configurations
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    else:
+        # testing environment configurations
+        app.config["TESTING"] = True
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_TEST_DATABASE_URI'] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
+
     db.init_app(app)
     migrate.init_app(app, db)
     # import crystal_bp from routes.py file
